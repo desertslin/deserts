405552f8a534d19a9e8d82465de03a6a0b3c64cc
zoea_g <zoea_g@qq.com>
2021-04-22 15:22:13 +0800

    删除添加

---
+++ src/main/java/com/deserts/oos/git/NewClass.java
@@ -0,0 +1,15 @@
+package com.deserts.oos.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @ClassName NewClass
+ * @Description TODO
+ * @Author deserts
+ * @Date 2021/4/22 15:21
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("new class");
+    }
+}
