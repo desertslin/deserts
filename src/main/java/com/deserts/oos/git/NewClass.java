098b6c1e37f9f96460d144fb726f34463ea5a84b
zoea_g <zoea_g@qq.com>
2021-04-22 16:14:11 +0800

    nnn

--- src/main/java/com/deserts/oos/git/NewClass.java
+++ src/main/java/com/deserts/oos/git/NewClass.java
@@ -12,5 +12,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("new class");
         System.out.println("dev3344");
+        System.out.println("nnn");
     }
 }
